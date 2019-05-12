d9c0f3cf9d3a223bbc258e637ca83a637cf05b1d
武二康 <bunikou@wuerkangdeMBP.lan>
2019-05-13 00:38:21 +0800

    端口号

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const cookieParser = require('cookie-parser')
 const apis = require('./apis')
 const {port} = require('../config')
 const mongoose = require('mongoose');
-const url = "mongodb://localhost:12180/mySpace";
+const url = "mongodb://master:wu154236@localhost:12180/mySpace";
 
 let connection = mongoose.connection
 connection.once('open', function () {
